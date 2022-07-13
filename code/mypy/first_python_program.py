e183988df8255c94786b351ae82870568bdcd0e6
Botpsycho <pkzander@protonmail.com>
2022-07-13 21:14:08 +0530

    clean up

--- code/mypy/first_python_program.py
+++
@@ -1,9 +0,0 @@
-age = input("What is your age: ")
-
-if int(age) >= 12:
-    print("You are not a kid anymore")
-    print("Be resposible")
-else:
-    print("You are kid go to your mama.")
-
-print("***********Ended***********")
--- code/mypy/little_dictionary.py
+++
@@ -1,2 +0,0 @@
-dic = {"perish":"die \n link=https://www.youtube.com/watch?v=dQw4w9WgXcQ", "mutable": "can be changed  \n https://www.youtube.com/watch?v=dQw4w9WgXcQ", "immutable":"cannot be changed \n https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-print("meaning:", dic[input("enter any of these words (perish, mutable, immutable): ")])
